f6d086979bb255e4e9c4a2c9b8bbc519113026ad
camiloojeda123 <Caojeda.4836@unicesmag.edu.co>
2025-04-03 11:22:49 -0500

    signin

--- src/signup.php
+++ src/signup.php
@@ -7,13 +7,37 @@
    $email = $_POST['e_mail'];
    $passw = $_POST['passw'];
    
+   //$enc_pass = md5($passwd);
+   $enc_pass =sha1($passw);
+
+   $sql_email_exist = "
+   SELECT 
+      COUNT(email) as total
+   FROM
+      users 
+   WHERE 
+     email = '$email' 
+     LIMIT 1 
+     ";
+   $res = pg_query($conn, $sql_email_exist);
+
+   if($res){
+    $row = pg_fetch_assoc($res);
+    if($row['total'] > 0){
+     echo "Email already exists";
+    }else{
+
     $sql = "INSERT INTO users (firstname, lastname, email, password)
-    VALUES('$fname','$lname','$email','$passwd')
+    VALUES('$fname','$lname','$email','$enc_pass')
     ";
+
     $res = pg_query($conn, $sql);
     if($res){
         echo "user has been created succesfully";
     }else{
         echo "error";
     }
+}
+   }
+
  ?>  
